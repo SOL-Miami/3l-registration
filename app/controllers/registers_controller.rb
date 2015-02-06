804c650589e28929e2952257f8d674f76e61a88d
Mark Natera <mark@truthkingdom.com>
2015-02-06 18:31:01 -0500

    done with scaffold

--- app/controllers/registers_controller.rb
+++ app/controllers/registers_controller.rb
@@ -1,5 +1,17 @@
 class RegistersController < InheritedResources::Base
   before_action :authenticate_admin_user!, only: [:index, :show, :edit, :update, :destroy]
+
+  def create
+    @register = Register.new(register_params)
+    if @register.save
+      redirect_to root_path
+    else
+      render :action => 'new'
+    end
+  end
+
+
+
   private
 
     def register_params
